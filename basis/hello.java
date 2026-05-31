a0e2fef3ef0e09896f98a3b3be0e4e9c0ce8bf37
Prat1012 <prateekhvrm2006@gmail.com>
2026-05-31 15:37:53 +0530

    changes

--- basis/hello.java
+++ basis/hello.java
@@ -1,5 +1,36 @@
 class hello{
+// this is the way to output the any text 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+         // star pattern {*}
+         System.out.println("*");
+         System.out.println("**" );
+         System.out.println("***");
+         System.out.println("****");
+         // 
+
+
+         // variable declaration and initialization
+            String Name ="Anushka";
+            int age = 20;
+            double price = 99.99;
+            System.out.println("Name: " + Name);
+            System.out.println("Age: " + age);  
+            System.out.println("Price: " + price);
+            //
+            // variables
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            System.out.println("Sum of a,b and c is : " + (a + b + c));
+            
+
+
+
+ 
+ 
+
+        
     }
 }
