761156f8dc7aa505cbe2937440ed1c40f106aca6
Prat1012 <prateekhvrm2006@gmail.com>
2026-06-01 15:44:04 +0530

    done with loops

--- basis/loops2.java
+++ basis/loops2.java
@@ -1,7 +1,77 @@
+import java.util.Scanner;
 public class loops2 {
     public static void main(String[] args) {
-    // for loop statement 
+                        // for loop statement 
     
     
-    }        
+     for (int i=0; i<5; i++){
+     System.out.println(i);
+    
+    }  
+   // print the  number from 1 to 20 
+   for (int i=0; i<=20;i++){
+    System.out.println(i);
+   } 
+//
+   
+                         // while loop statement 
+// while (condition){
+       // code to be executed
+       //}
+
+
+// while means jab tk statement true hoga tab tk loop chalega
+   int i =0;
+   while (i<5){
+    System.out.println(i);
+    i++;
+   }
+
+   // do-while loop statement 
+   // do{
+    // code to be executed}
+// while (condition);
+// isme pehle do m btate h kya kaam krna h phir condition check krta h ki loop chalega ya nhi
+do{
+System.out.println("This is do while loop");
+}
+while (false);
+
+// print the sum of  first  n  natural number 
+System.out.println("Enter the value of n : ");
+Scanner Num = new Scanner (System.in);
+int n = Num.nextInt();
+int sum =0;
+for(int z =1;z<=n;z++){
+    sum =sum +z;
+
+}
+System.out.println("the Sum of First Natural Number are :"+sum);
+
+// print the multiplication table of a given number
+System.out.println("Enter the number you want to print the multiplication table : ");
+int No =Num.nextInt();
+ for(int x=1;x<=11;x++){
+    System.out.println(No*x);
+
+ }
+
+
+   
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 }
