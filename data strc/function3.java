96fe6ac03dcf0212708caed7304903247b19dbe5
Prat1012 <prateekhvrm2006@gmail.com>
2026-06-09 15:56:15 +0530

    done

---
+++ data strc/function3.java
@@ -0,0 +1,15 @@
+public class function3 {
+
+    public static int Product(int a , int b){
+        pro= a*b;
+        return Pro;
+    }
+
+
+
+
+    public static void main(String[] args){
+System.out.println("enter your number");
+ 
+    }
+}
--- data strc/functions2.java
+++ data strc/functions2.java
@@ -1,20 +1,20 @@
-// import java.util.Scanner;
-// public class functions2 {
+import java.util.Scanner;
+public class functions2 {
   
 
-//     public static int Calculate (int a ,int b){
-//   int sum = a+b;
-//   return sum;
-//     }
+    public static int Calculate (int a ,int b){
+  int sum = a+b;
+  return sum;
+    }
     
-//     public static void main(String[] args) {
-//       Scanner sc = new Scanner(System.in);
-//      int a = sc.nextInt();
-//      int b  = sc.nextInt();
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     int b  = sc.nextInt();
 
-// int sum =Calculate(a,b);
-// System.out.println(sum);
-//     }
-// }
+int sum =Calculate(a,b);
+System.out.println(sum);
+    }
+}
   
 
